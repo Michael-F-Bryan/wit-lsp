357f896003a0be71a170de2171b7d412b27b4544
Michael-F-Bryan <michaelfbryan@gmail.com>
2024-05-06 23:17:50 +0800

    Changed `Text`'s `Debug` implementation to use escape sequences

--- crates/wit-compiler/src/text.rs
+++ crates/wit-compiler/src/text.rs
@@ -136,7 +136,7 @@ impl Display for Text {
 
 impl Debug for Text {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(self.as_str())
+        Debug::fmt(&self.0, f)
     }
 }
 
